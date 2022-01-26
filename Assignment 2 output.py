b219f91e61b3de8b63184d900e14d38e78fee787
bt21104125 <97115865+bt21104125@users.noreply.github.com>
2022-01-26 16:52:22 +0530

    Add files via upload

---
+++ Assignment 2 output.py
@@ -0,0 +1,54 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: C:\Users\KESHAV SINGH\AppData\Local\Programs\Python\Python310\Assignment 2 Code.py ==========
+Solution of Ques.No.1 Using Different string operations
+ 
+35
+ 
+egaugnal evitisnes esac a si nohtyP
+ 
+['', ' case sensitive language']
+ 
+Python is object oriented language
+ 
+10
+ 
+Pythonisacasesensitivelanguage
+ 
+Solution of Ques.No.2 Storing different variables in the string after formatting
+Enter the NAME of student=Keshav Singh
+Enter the SID of student=21104125
+Enter the DEPARTMENT NAME of student=Electrical Engineering
+Enter the CGPA of student=9.9
+Hey Keshav Singh Here !
+My SID is 21104125
+I am from Electrical Engineering department and my CGPA is 9.9
+ 
+Solution of Ques.No.3 Using bitwise operator to calculate the following
+8
+ 
+58
+  
+50
+ 
+2
+ 
+160
+ 
+Solution of Ques.No.4 Finding Greatest Number among he three entered number  
+Enter the First number=24
+Enter the Second number=26
+Enter the Third number=24
+b is the greatest number among three number
+ 
+Solution of Ques.No.5 Finding word(name) in the entered string by the user
+Enter any String=My name is Keshav Singh
+Yes
+ 
+Solution of Ques.No.6 Test to form a triangle by entered 
+Enter the First side of triangle=13
+Enter the Second side of triangle=12
+Enter the Third side of triangle=5
+Triangles is formed
+ 
